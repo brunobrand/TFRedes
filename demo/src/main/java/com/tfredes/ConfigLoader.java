62bd80bff4abbfe9070de8b72967ad31ac58273e
Bruno Brandão <bruno.brandao@ad.ufrgs.br>
2025-06-05 15:37:02 -0300

    first classes and project structure

---
+++ demo/src/main/java/com/tfredes/ConfigLoader.java
@@ -0,0 +1,43 @@
+package com.tfredes;
+import java.io.bufferedReader;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class ConfigLoader {
+    private String nextNodeIP;
+    private int nextNodePort;
+    private String nickname;
+    private int tokenHoldTime;
+    private boolean isTokenGenerator;
+
+    public ConfigLoader(String fileName) {
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        String[] ipPort = reader.readLine().split(":");
+        this.nextNodeIP = ipPort[0];
+        this.nextNodePort = Integer.parseInt(ipPort[1]);
+        this.nickname = reader.readLine();
+        this.tokenHoldTime = Integer.parseInt(reader.readLine());
+        this.isTokenGenerator = Boolean.parseBoolean(reader.readLine());
+        reader.close();
+    }
+
+    public String getNextNodeIP() {
+        return nextNodeIP;
+    }
+
+    public int getNextNodePort() {
+        return nextNodePort;
+    }	
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public int getTokenHoldTime() {
+        return tokenHoldTime;
+    }
+
+    public boolean isTokenGenerator() {
+        return isTokenGenerator;
+    }   
+}
